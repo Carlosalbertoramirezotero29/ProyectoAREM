cd0419c07620fd3cf1ea7cee012f068b37616391
Ricardo Pinto <ricardo.pinto@mail.escuelaing.edu.co>
2017-05-09 14:58:34 -0500

    App heroku

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,8 +3,14 @@ import static spark.Spark.*;
 public class Main {
 
     public static void main(String[] args) {
+        DaoProducts daopro = new DaoProducts();
+        DaoDataBase daodata= new DaoDataBase();
         port(getHerokuAssignedPort());
-        get("/hello", (req, res) -> "Hello Heroku World");
+        get("/cotizacion/:genero/:pijama", (req, res) -> "El precio solicitado es: "+daopro.getValueProduct(req.params(":genero").toString(), req.params(":pijama").toString()));
+        get("/clientes", (req, res) -> "Nuestros clientes: <br>"+daodata.getClients());
+        get("/cotizacion/:genero", (req, res) -> "Pijamas: <br>"+daopro.getPijamas(req.params(":genero").toString()));
+
+
     }
 
     static int getHerokuAssignedPort() {
